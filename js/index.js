95b2d1f04e52802b550edb4e70b370be702abb8e
David <dave.j.hazelwood@gmail.com>
2021-05-18 19:10:25 -0500

    Added the asteroid

--- js/index.js
+++ js/index.js
@@ -3,6 +3,24 @@ let canvasElement = document.getElementById("spaceGameCanvas");
 let ctx = canvasElement.getContext("2d");
 let canvasWidth = canvasElement.width;
 let canvasHeight = canvasElement.height;
+let x = canvasWidth/2;
+let y = canvasHeight/2;
+
+//Asteroid variables
+let asteroidImage = new Image(32, 32);
+asteroidImage.src = './img/AsteroidImage.png';
+let asteroidRadius = 10;
+let dx = 2;
+let dy = 2;
+
+//Adds the asteroids to the game
+function drawAsteroid() {
+    ctx.beginPath()
+    ctx.arc(x, y, asteroidRadius, 0, Math.PI*2);
+    ctx.src = asteroidImage;
+    ctx.fill();
+    ctx.closePath();
+}
 
 //Player Image
 let playerImage = new Image(32, 32);
@@ -21,11 +39,15 @@ function clear() {
     ctx.clearRect(0, 0, 1024, 576);
 }
 
+//Tracks mouse
 canvasElement.addEventListener('mousemove', function(event) {
    mouse.x = event.x - canvasPosition.left;
    mouse.y = event.y - canvasPosition.top;
-   clear();
-   ctx.drawImage(playerImage, mouse.x, mouse.y);
+
+});
+
+//Shoots laser when mouse clicked
+canvasElement.addEventListener('mousedown', function(event) {
 
 });
 
@@ -34,3 +56,22 @@ canvasElement.addEventListener('mousemove', function(event) {
 canvasElement.addEventListener('mousedown', function(event) {
 
 });
+
+function draw() {
+    clear();
+    drawAsteroid();
+    if(x + dx > canvasWidth-asteroidRadius || x + dx < asteroidRadius) {
+        dx = -dx;
+    }
+
+    if(y + dy < asteroidRadius) {
+        dy = -dy;
+    }
+
+    x += dx;
+    y += dy;
+}
+
+draw();
+
+let interval = setInterval(draw, 10);
